545ad4061508b9fea3a4f32df39bd83df15fff0a
Joseph O'Reilly <zebone29@gmail.com>
2014-11-05 13:53:05 +0000

    app.js edited

--- public/js/app.js
+++ public/js/app.js
@@ -1,31 +1,4 @@
-// (function() {
-//   var app = angular.module('registerApp', []);
 
-//   app.controller('RegisterController', function(){
-
-//       this.users = AllUsers;
-//   });
-
-
-// app.controller('AddUser', function(){
-
-//   this.AllUsers = {};
-  
-//   this.addUser = function(user){
-//     AllUsers.push(this.user);
-//     //console.log("hello from AddComment");
-//      console.log(user);
-//      };
-//  });
-
-
-// var AllUsers=[{
-//   email: 'q@mail.com',
-//   password: 'itsmebdy'
-// }];
-
-
-//   })();
 
 
   (function() {
@@ -35,10 +8,10 @@
 
       var _this = this;
       
-      _this.users = [{
-      email: 'q@mail.com',
-      password: 'itsmebdy'
-      }];
+      // _this.users = [{
+      // email: 'q@mail.com',
+      // password: 'itsmebdy'
+      // }];
 
 
       $http.get('/api/user').success(function(data){
@@ -52,9 +25,16 @@
       this.user = {};
   
       this.addUser = function(user){
-         _this.users.push(this.user);
+
+        $http.post('/api/user', user).success(function(data){
+        _this.users = data;
+      })
+      .error(function(data){
+        console.log('Error');
+      });
+        // _this.users.push(this.user);
         //console.log("hello from AddComment");
-         console.log(users);
+         //console.log(users);
      };//addUser
 
 
